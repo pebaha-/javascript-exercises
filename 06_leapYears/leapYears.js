a7f0c4793a60bbb64b7d2da232fb756273e68dbd
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-13 00:31:19 +0200

    Split case into two

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,7 +2,10 @@ const leapYears = function (year) {
     if (year % 400 == 0) {
         return true;
     }
-    else if (year % 4 == 0 && year % 100 != 0) {
+    else if (year % 100 == 0)  {
+        return false;
+    }
+    else if (year % 4 == 0) {
         return true;
     }
     return false;
