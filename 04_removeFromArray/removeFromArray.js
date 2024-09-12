ecbe3d96c7378240cdc88fe126681469456f018a
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-12 23:47:20 +0200

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (valuesArray, ...valuesToRemove) {
+    for (i = valuesArray.length - 1; i >= 0; i--) {
+        for (j = 0; j < valuesToRemove.length; j++) {
+            if (valuesArray[i] === valuesToRemove[j]) {
+                valuesArray.splice(i, 1);
+            }
+        }
+    }
+    return valuesArray;
 };
 
 // Do not edit below this line
