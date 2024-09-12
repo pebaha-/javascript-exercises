c2e3c6947a59a60d740fbc54264a86fb625f44d4
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-12 21:40:19 +0200

    Add solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    let repeatedString = "";
+    for (i = 0; i < repeats; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
