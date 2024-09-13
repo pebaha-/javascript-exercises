d7ea02c847a992b791e8b39dbb9eaff9f22ef7f9
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-13 11:37:32 +0200

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function (number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function (fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5 / 9
+  return roundToOneDecimal(celcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let fahrenheit = (celcius * 9 / 5) + 32;
+  return roundToOneDecimal(fahrenheit);
 };
 
 // Do not edit below this line
