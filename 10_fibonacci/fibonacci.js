943c03d069a7ce0a000287c2cf66ba44110ecbc1
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-22 16:22:01 +0200

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  if (x < 0) return 'OOPS';
+  if (x == 0) return 0;
+  return x <= 2 ? 1 : fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
