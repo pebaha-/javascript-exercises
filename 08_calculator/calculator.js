b42347b569affdf7fb0b12dff8597d028cda9a7e
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-22 14:29:22 +0200

    Add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+  return a + b;
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const factorial = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 };
+
+const multiply = function (arr) {
+  return arr.reduce((product, current) => product * current, 1);
+};
+
+const power = function (a, b) {
+  return a ** b;
+};
+
+const factorial = function (a) {
+  let fact = 1;
+  for (i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
+};
+
+// Alternative factorial solution
+const factorialWithArrayMethods = function (a) {
+  return Array.from({ length: a }, (_, i) => i + 1).reduce((fact, current) => fact * current, 1);
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +39,6 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
+  factorialWithArrayMethods
 };
