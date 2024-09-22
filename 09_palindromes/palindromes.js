e86bc8e3ca09782968a8d37c5bf2394f09c925e5
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-22 16:12:02 +0200

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str
+        .replaceAll(' ', '')
+        .replaceAll('!', '')
+        .replaceAll(',', '')
+        .replaceAll('.', '')
+        .toLowerCase();
+    return str === str.split('').reverse().join('');
 };
 
 // Do not edit below this line
