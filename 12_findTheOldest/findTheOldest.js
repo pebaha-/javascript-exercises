291a3fd2a73300cad30014215567f521370db79f
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-22 17:04:42 +0200

    Add solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr
+    .map((person) => {
+      return { 'yearOfDeath': new Date().getFullYear(), ...person }
+    })
+    .sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0];
 };
 
 // Do not edit below this line
